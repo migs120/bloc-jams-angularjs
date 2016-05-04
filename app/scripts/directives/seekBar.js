fce05b0787a2b273bc4218b581a25769160db0d1
Your Name <you@example.com>
2016-05-04 11:03:24 -0700

    angcheck-9done

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,4 +1,5 @@
- (function() {
+ 
+(function() {
     function seekBar($document) {
          
                     var calculatePercent = function(seekBar, event) {
@@ -7,7 +8,16 @@
                                                                      var offsetXPercent = offsetX / seekBarWidth;
                                                                      offsetXPercent = Math.max(0, offsetXPercent);
                                                                      offsetXPercent = Math.min(1, offsetXPercent);
+                                                                     console.log(
+                                                                                 "seekBar.offset().left =>",seekBar.offset().left
+                                                                                 ,"\n event.pageX =>",event.pageX
+                                                                                 ,"\n offsetX =>",offsetX
+                                                                                 ,"\n seekBarWidth =>",seekBarWidth
+                                                                                 ,"\n offsetXPercent =>",offsetXPercent
+                                                                                
+                                                                                )
                                                                      return offsetXPercent;
+                                                                   
                                                                    };
                       return {
                              templateUrl: '/templates/directives/seek_bar.html',                                                                                                   
@@ -24,23 +34,41 @@
                                                                                                           var percent = value / max * 100;
                                                                                                           return percent + "%";
                                                                                                          };
+                                                                         scope.thumbStyle = function(){ 
+                                                                                                       return {left: percentString()}
+                                                                                                       
+                                                                             
+                                                                                                      }
 
                                                                          scope.fillStyle = function() {
+                                                                                                       console.log(    
+                                                                                                                    //"fillStyle \n ->width: percentString() \n"
+                                                                                                                    //,"ng-style= 'width:" + percentString() +"'" 
+                                                                                                                   )
+                                                                                                       // \|/ this returns to ng-style as CSS changer
                                                                                                        return {width: percentString()};
                                                                                                      };
-                                                                        scope.onClickSeekBar = function(event) {
+                                                                         scope.onClickSeekBar = function(event) {                            //  \|/ -> var seekBar = $(element)
                                                                                                                  var percent = calculatePercent(seekBar, event);
                                                                                                                  scope.value = percent * scope.max;
+                                                                                                                 console.log(
+                                                                                                                              "onClickSeekBar \n -> scope.value",scope.value
+                                                                                                                             ,"\n onClickSeekBar \n -> argument(event)",event
+                                                                                                                             //,"\n seekBar = $(element) ->",seekBar  // jqSelector forCurrent seekbarElement
+                                                                                                                            
+                                                                                                                            )
                                                                                                                };
                                  
-                                                                        scope.trackThumb = function() {
+                                                                         scope.trackThumb = function() { 
+                                                                             //<div class="thumb" ng-mousedown="trackThumb()"></div>
                                                                                                          $document.bind(
                                                                                                                         'mousemove.thumb', function(event) {
                                                                                                                                                              var percent = calculatePercent(seekBar, event);
-                                                                                                                                                             scope.$apply(function() {
-                                                                                                                                                                                     scope.value = percent * scope.max;                               
-                                                                                                                                                                                     }
-                                                                                                                                                                        );
+                                                                                                                                                            // scope.$apply(function() {
+                                                                                                                                                                                     scope.value = percent * scope.max;
+                                                                                                                                                                                      
+                                                                                                                                                                                   //  }
+                                                                                                                                                                      //  );
                                                                                                                                                           }
                                                                                                                        );
 
@@ -49,7 +77,7 @@
                                                                                                                                                      $document.unbind('mousemove.thumb');
                                                                                                                                                      $document.unbind('mouseup.thumb');
                                                                                                                                                    }
-                                                                                                                       );
+                                                                                                                       ); 
                                                                                                      };
                                  
                                                                         }
@@ -60,3 +88,6 @@
          .module('blocJams')
         .directive('seekBar', ['$document', seekBar]);
  })();
+ 
+
+
